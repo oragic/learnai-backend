a34c842615a2991b36f5d119d056b3b1a9428863
Rai D Alves <67238752+oragic@users.noreply.github.com>
2025-03-15 23:40:48 -0300

    feat: :sparkles: Implement flashcard port

--- internal/core/port/flashcard.go
+++ internal/core/port/flashcard.go
@@ -1,7 +1,20 @@
 package port
 
+import (
+	"context"
+	"learnai/internal/core/domain"
+)
+
 type FlashCardRepository interface {
+	CreateFlashCard(ctx context.Context, flashcard *domain.Flashcard) (*domain.Flashcard, error)
+	GetFlashCardByID(ctx context.Context, id uint64) (*domain.Flashcard, error)
+	GetFlashCard(ctx context.Context, id uint64) ([]domain.Flashcard, error)
+	DeleteFlashCard(ctx context.Context, id uint64) error
 }
 
 type Flashcard interface {
+	CreateFlashCard(ctx context.Context, flashcard *domain.Flashcard) (*domain.Flashcard, error)
+	GetFlashCard(ctx context.Context, id uint64) ([]domain.Flashcard, error)
+	GetFlashCardByID(ctx context.Context, id uint64) (*domain.Flashcard, error)
+	DeleteFlashCard(ctx context.Context, id uint64) error
 }
