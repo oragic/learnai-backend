1f9eb01231b2b0b0d284a83eeb0d4ab9af82cbc6
Rai D Alves <67238752+oragic@users.noreply.github.com>
2025-03-16 17:30:04 -0300

    refactor: :wrench: insert function that create configuration instance

--- internal/adapters/config/config.go
+++ internal/adapters/config/config.go
@@ -1,6 +1,11 @@
 package config
 
+import "os"
+
 type (
+	Container struct {
+		DB *DB
+	}
 	DB struct {
 		Connection string
 		Host       string
@@ -10,3 +15,16 @@ type (
 		Name       string
 	}
 )
+
+func New() (*Container, error) {
+	DB := &DB{
+		Connection: os.Getenv("DB_CONNECTION"),
+		Host:       os.Getenv("DB_HOST"),
+		Port:       os.Getenv("DB_PORT"),
+		User:       os.Getenv("DB_USER"),
+		Password:   os.Getenv("DB_PASSWORD"),
+		Name:       os.Getenv("DB_NAME"),
+	}
+
+	return &Container{DB: DB}, nil
+}
