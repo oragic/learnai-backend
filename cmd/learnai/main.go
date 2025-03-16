82cf9662b11c5cca82fb2b5ac87922b9cdb1c29a
Rai D Alves <67238752+oragic@users.noreply.github.com>
2025-03-16 18:32:13 -0300

    feat: :sparkles: init db instance in entry point application

--- cmd/learnai/main.go
+++ cmd/learnai/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"learnai/internal/adapters/config"
+	"learnai/internal/adapters/storage/postgres"
+	"log/slog"
+	"os"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+	config, err := config.New()
+	if err != nil {
+		slog.Error("Error loading environment variables", "error", err)
+		os.Exit(1)
+	}
+
+	ctx := context.Background()
+	db, err := postgres.New(ctx, *config.DB)
+	if err != nil {
+		slog.Error("Error connecting to the database", "error", err)
+		os.Exit(1)
+	}
+
+	defer db.Close()
 }
