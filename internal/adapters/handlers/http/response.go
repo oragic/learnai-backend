9782aab149a8fe89f775fdf6f520140ba59b8b41
Rai D Alves <67238752+oragic@users.noreply.github.com>
2025-03-22 23:14:43 -0300

    feat: :sparkles: implement response flow

--- internal/adapters/handlers/http/response.go
+++ internal/adapters/handlers/http/response.go
@@ -1,7 +1,89 @@
 package http
 
+import (
+	"errors"
+	"learnai/internal/core/domain"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
 type response struct {
 	Success bool   `json:"success" example:"true"`
 	Message string `json:"message" example:"Success"`
 	Data    any    `json:"data,omitempty"`
+}
+
+func newResponse(success bool, message string, data any) response {
+	return response{
+		Success: success,
+		Message: message,
+		Data:    data,
+	}
+}
+
+var errorStatusMap = map[error]int{
+	domain.ErrInternal:                   http.StatusInternalServerError,
+	domain.ErrDataNotFound:               http.StatusNotFound,
+	domain.ErrConflictingData:            http.StatusConflict,
+	domain.ErrInvalidCredentials:         http.StatusUnauthorized,
+	domain.ErrUnauthorized:               http.StatusUnauthorized,
+	domain.ErrEmptyAuthorizationHeader:   http.StatusUnauthorized,
+	domain.ErrInvalidAuthorizationHeader: http.StatusUnauthorized,
+	domain.ErrInvalidAuthorizationType:   http.StatusUnauthorized,
+	domain.ErrInvalidToken:               http.StatusUnauthorized,
+	domain.ErrExpiredToken:               http.StatusUnauthorized,
+	domain.ErrForbidden:                  http.StatusForbidden,
+	domain.ErrNoUpdatedData:              http.StatusBadRequest,
+	domain.ErrInsufficientStock:          http.StatusBadRequest,
+	domain.ErrInsufficientPayment:        http.StatusBadRequest,
+}
+
+func validationError(ctx *gin.Context, err error) {
+	errMsgs := parseError(err)
+	errRsp := newErrorResponse(errMsgs)
+	ctx.JSON(http.StatusBadRequest, errRsp)
+}
+
+func handleError(ctx *gin.Context, err error) {
+	statusCode, ok := errorStatusMap[err]
+	if !ok {
+		statusCode = http.StatusInternalServerError
+	}
+
+	errMsg := parseError(err)
+	errRsp := newErrorResponse(errMsg)
+	ctx.JSON(statusCode, errRsp)
+}
+
+func parseError(err error) []string {
+	var errMsgs []string
+
+	if errors.As(err, &validator.ValidationErrors{}) {
+		for _, err := range err.(validator.ValidationErrors) {
+			errMsgs = append(errMsgs, err.Error())
+		}
+	} else {
+		errMsgs = append(errMsgs, err.Error())
+	}
+
+	return errMsgs
+}
+
+type errorResponse struct {
+	Success bool     `json:"success"  example:"false"`
+	Message []string `json:"message" example:"Error message 1, Error message 2"`
+}
+
+func newErrorResponse(errMsgs []string) errorResponse {
+	return errorResponse{
+		Success: false,
+		Message: errMsgs,
+	}
+}
+
+func handleSuccess(ctx *gin.Context, data any) {
+	rsp := newResponse(true, "Success", data)
+	ctx.JSON(http.StatusOK, rsp)
 }
